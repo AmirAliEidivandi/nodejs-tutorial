0b6bd4e987b2c526ddfbde1bea46c3acd2c7d9ed
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-16 20:39:45 +0430

    complete ejs tutorial

--- folder_3/index.js
+++ folder_3/index.js
@@ -1,28 +1,18 @@
 const path = require("path");
 const express = require("express");
-const exphbs = require("express-handlebars");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.engine(
-    "hbs",
-    exphbs.engine({
-        extname: "hbs",
-        layoutsDir: "views/layouts",
-        defaultLayout: "main",
-        partialsDir: "views/partials",
-    })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (req, res) => {
     const arr = [{ name: "amirali" }, { name: "amir" }, { name: "ali" }];
 
-    res.render("index", { pageTitle: "amir", names: arr, home: true, hasName: arr.length > 0 });
+    res.render("index", { pageTitle: "amir", names: arr });
 });
 
 const PORT = process.env.PORT || 8000;
