dc24368e8077989f4822367f0de48e2acf6d49c9
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-16 23:39:06 +0430

    create post todo

---
+++ folder_4/utils/statics.utils.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require("express");
+
+exports.setStatic = (app) => {
+    app.use(express.static(path.join(__dirname, "..", "public")));
+    app.use(express.static(path.join(__dirname, "..", "node_modules", "bootstrap-v4-rtl", "dist")));
+    app.use(express.static(path.join(__dirname, "..", "node_modules", "font-awesome")));
+};
