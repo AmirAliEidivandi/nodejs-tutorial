8ff029bc469c0259574dec64175b677c30607897
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-20 19:26:50 +0430

    completed crud

--- folder_4/controllers/todo.controller.js
+++ folder_4/controllers/todo.controller.js
@@ -1,41 +1,52 @@
 const Todo = require("../models/todo.model");
-const { generateRandomId, getRemainingTodos, getCompletedTodo } = require("../utils/todos.utils");
 
 exports.addTodo = (req, res) => {
     if (!req.body.todo) return res.redirect("/");
 
-    const todo = new Todo(generateRandomId(), req.body.todo);
-    todo.save((err) => {
-        if (!err) res.redirect("/");
-        console.log(err);
-    });
+    Todo.create({ text: req.body.todo })
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res) => {
-    getCompletedTodo((completedTodo) => {
-        getRemainingTodos((remainingTodo) => {
-            Todo.fetchAll((todos) => {
+    Todo.count({
+        where: {
+            completed: true,
+        },
+    }).then((completedTodos) => {
+        Todo.findAll()
+            .then((todos) => {
                 res.render("index", {
                     pageTitle: "todo-list",
                     todos,
-                    completedTodo,
-                    remainingTodo,
+                    completedTodos,
+                    remainingTodo: todos.length - completedTodos,
                 });
-            });
-        });
+            })
+            .catch((err) => console.log(err));
     });
 };
 
 exports.deleteTodo = (req, res) => {
-    Todo.deleteTodo(req.params.id, (err) => {
-        if (!err) res.redirect("/");
-        console.log(err);
-    });
+    Todo.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.completeTodo = (req, res) => {
-    Todo.setTodoToComplete(req.params.id, (err) => {
-        if (!err) res.redirect("/");
-        console.log(err);
-    });
+    Todo.findByPk(req.params.id)
+        .then((todo) => {
+            todo.completed = true;
+            return todo.save();
+        })
+        .then(() => res.redirect("/"))
+        .catch((err) => console.log(err));
 };
