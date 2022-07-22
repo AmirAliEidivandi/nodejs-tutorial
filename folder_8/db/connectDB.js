3169e92610a6d9fddb87d5eef9e1c001881266cb
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-22 21:16:22 +0430

    config database

---
+++ folder_8/db/connectDB.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async (uri) => {
+    try {
+        const conn = await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
