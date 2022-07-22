8c68bbc4cff76e5037120a0d9b823684769d1632
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-22 22:25:33 +0430

    setup bootstrap and font-awesome

--- folder_8/app.js
+++ folder_8/app.js
@@ -3,9 +3,12 @@ const app = express();
 require("dotenv").config({ path: "./config/config.env" });
 const homeRoute = require("./routes/home.routes");
 
+// env
+const { MONGO_URI, PORT, NODE_ENV, BOOTSTRAP, FONT_AWESOME } = process.env;
+
 // middleware
-require("./middlewares/index.middleware")(app);
-require("./db/connectDB")(process.env.MONGO_URI);
+require("./middlewares/index.middleware")(app, BOOTSTRAP, FONT_AWESOME);
+require("./db/connectDB")(MONGO_URI);
 
 // view engine
 app.set("view engine", "ejs");
@@ -14,5 +17,4 @@ app.set("views", "views");
 // routes
 app.use(homeRoute);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Example app listening in ${process.env.NODE_ENV} mode on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Example app listening in ${NODE_ENV} mode on port ${PORT}!`));
--- folder_8/middlewares/index.middleware.js
+++ folder_8/middlewares/index.middleware.js
@@ -2,10 +2,12 @@ const path = require("path");
 const morgan = require("morgan");
 const express = require("express");
 
-module.exports = (app) => {
+module.exports = (app, BOOTSTRAP, FONT_AWESOME) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
-    app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.static(path.join(__dirname, "..", "public")));
+    app.use(express.static(path.join(__dirname, BOOTSTRAP)));
+    app.use(express.static(path.join(__dirname, FONT_AWESOME)));
 
     if (process.env.NODE_ENV === "development") {
         app.use(morgan("dev"));
