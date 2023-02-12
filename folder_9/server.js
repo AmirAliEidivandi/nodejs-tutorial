75aa5206ab48bf3b8eef27e403fe860e4d6aa838
AmirAliEidivandi <amirah648887@gmail.com>
2023-02-13 01:53:01 +0330

    test commit

--- folder_9/server.js
+++ folder_9/server.js
@@ -4,12 +4,12 @@ const port = 3000;
 const handleCallback = require("./middleware/handle.error");
 
 app.get(
-    "/myroute",
-    handleCallback(async (req, res) => {
-        const myError = new Error("My error!");
-        myError.code = 500;
-        throw myError;
-    })
+  "/myroute",
+  handleCallback(async (req, res) => {
+    const myError = new Error("My error!");
+    myError.code = 500;
+    throw myError;
+  })
 );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
